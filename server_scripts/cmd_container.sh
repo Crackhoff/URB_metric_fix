4ed4c3f7e449c3f04defc11eb843b930b139391e
aonurakman <ahmetonurakman@gmail.com>
2025-04-08 21:57:36 +0200

    Adding server scripts

---
+++ server_scripts/cmd_container.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd /app/
+echo "--- Installing dependencies ---"
+pip3 install --no-cache-dir -r requirements.txt
+echo "--- Running main.py ---"
+python -u scripts/ippo_torchrl.py --id 1 --conf 1_ippo --net gargenville --seed 42
