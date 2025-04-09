a0829842eb5f016b4da894427b04ba16b6b0589a
aonurakman <ahmetonurakman@gmail.com>
2025-04-09 11:09:45 +0200

    container cmd update

--- server_scripts/cmd_container.sh
+++ server_scripts/cmd_container.sh
@@ -2,6 +2,6 @@
 
 cd /app/
 echo "--- Installing dependencies ---"
-pip3 install --no-cache-dir -r requirements.txt
+pip3 install --force-reinstall --no-cache-dir -r requirements.txt
 echo "--- Running main.py ---"
 python -u scripts/ippo_torchrl.py --id 1 --conf 1_ippo --net gargenville --seed 42
