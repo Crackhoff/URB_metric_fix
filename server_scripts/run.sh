8da4d8008ce1365e1eca9dee1462ae0d0ac98a58
aonurakman <ahmetonurakman@gmail.com>
2025-04-15 09:52:31 +0200

    server script modification

--- server_scripts/run.sh
+++ server_scripts/run.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-#SBATCH --job-name=urb_1
+#SBATCH --job-name=urb
 #SBATCH --qos=big
 #SBATCH --mem=64G
 #SBATCH --cpus-per-task=4
 #SBATCH --gres=gpu:1
 #SBATCH --partition=dgx
 
-PATH_PROGRAM="/home/$USER/URB"
+FOLDER_NAME="URB"
+PATH_PROGRAM="/home/$USER/$FOLDER_NAME"
 PUT_PROGRAM_TO="/app"
 PATH_SUMO_CONTAINER="/shared/sets/singularity/sumo.sif"
-CMD_PATH="/home/$USER/URB/server_scripts/cmd_container.sh"
-PRINTS_SAVE_PATH="/home/$USER/URB/server_scripts/container_printouts/output_$SLURM_JOB_ID.txt"
+CMD_PATH="$PATH_PROGRAM/server_scripts/cmd_container.sh"
+PRINTS_SAVE_PATH="$PATH_PROGRAM/server_scripts/container_printouts/output_$SLURM_JOB_ID.txt"
 
 mkdir -p "$(dirname "$PRINTS_SAVE_PATH")"
 # Run container by adding code by binding, run commands from cmd_container.sh, save printouts to a file
