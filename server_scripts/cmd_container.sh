28f1b6e8bbe2d2c6e05e9ef780b37f0bf410c4cc
aonurakman <ahmetonurakman@gmail.com>
2025-05-14 01:11:36 +0200

    Deleting server_scripts

--- server_scripts/cmd_container.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /app/
-python3 -m venv /tmp/venv
-source /tmp/venv/bin/activate
-echo "--- Installing dependencies ---"
-pip3 install --force-reinstall --no-cache-dir -r requirements.txt
-echo "--- Running main.py ---"
-python3 -u scripts/ippo_torchrl.py --id 1 --conf 1_ippo --net gargenville --seed 42
